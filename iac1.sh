9c51bf193f4a7ad9a09b8170f577640670ae3c03
rafaelmachado19 <107668946+rafaelmachado19@users.noreply.github.com>
2022-11-15 20:24:21 -0300

    Update iac1.sh

--- iac1.sh
+++ iac1.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+
+#Author: Rafael Machado
+#	Version: v1.0.0
+#	Description: SCRIPT SIMPLES PARA CRIAR USUÁRIOS, DIRETÓRIOS, GRUPOS E PERMISSÕES. 
+
+
+#/////////////////CRIANDO DIRETÓRIOS///////////////////#
 echo "Criando diretórios..."
 
 mkdir /publico /adm /ven /sec
 
+#////////////////CRIANDO GRUPOS////////////////////////#
 echo "Criando grupos de usuários..."
 
 groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 
+#////////////////CRIANDO USUÁRIOS E ATRIBUINDO EM SEUS GRUPOS////////////////////#
 echo "Criando usuários..."
 
 useradd carlos -ms /bin/bash -p "$(openssl passwd -crypt Senha123)" -G GRP_ADM
@@ -26,11 +35,14 @@ useradd josefina -ms /bin/bash -p "$(openssl passwd -crypt Senha123)" -G GRP_SEC
 useradd amanda -ms /bin/bash -p "$(openssl passwd -crypt Senha123)" -G GRP_SEC
 useradd rogerio -ms /bin/bash -p "$(openssl passwd -crypt Senha123)" -G GRP_SEC
 
+#///////////////ESPECIFICANDO PERMISSÕES AOS DIRETORIOS/////////////////////#
 echo "Especificando permissões dos diretorios..."
 
 chown root:GRP_ADM /adm
 chown root:GRP_VEN /ven
 chown root:GRP_SEC /sec
+
+#///////////ESPECIFICANDO ACESSO AOS DIRETÓRIOS///////////////////////////#
 
 chmod 770 /adm
 chmod 770 /ven
